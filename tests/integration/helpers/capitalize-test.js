4097805afd23ff356287175629bec0b876f3dcab
Sean Devine <sean-devine@x-b-e.com>
2021-02-16 22:19:30 -0600

    Update all imports of htmlSafe and isHtmlSafe

--- tests/integration/helpers/capitalize-test.js
+++ tests/integration/helpers/capitalize-test.js
@@ -2,7 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import { htmlSafe } from '@ember/string';
+import { htmlSafe } from '@ember/template';
 
 module('Integration | Helper | {{capitalize}}', function(hooks) {
   setupRenderingTest(hooks);
